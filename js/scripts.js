407f9cb5ca5f40c0402919d09c6ce2fe2c201515
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-09-12 14:38:14 +0300

    add the toggle fuction for what we do

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
       $(document).ready(function() {
-        $(".clickable").click(function() {
-          $(".initially-showing").toggle();
-          $(".initially-hidden").toggle();
+
+        //design click
+        $(".designclick").click(function() {
+          $("#initially-showing").toggle();
+          $("#initially-hidden").toggle();
+        });
+
+          //development click
+          $(".developmentclick").click(function() {
+          $("#start-showing").toggle();
+          $("#start-hidden").toggle();
+        });
+
+
+          //product management click
+          $(".productmanagement").click(function() {
+          $("#fast-showing").toggle();
+          $("#fast-hidden").toggle();
         });
       });
     

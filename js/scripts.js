3c7fdd4b78d45b9ad2cc9c3d582a2d480b901101
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-09-15 16:41:32 +0300

    edit on the mail chip

--- js/scripts.js
+++ js/scripts.js
@@ -67,9 +67,5 @@
                 $(".myfirstwork8").hide();
             });
 
-           $(".form-control").click(function() {
-          $("#initially-showing").alert("johndoe we have received your message. Thank you fro reaching out to us.");
-        });
-
       });
   
